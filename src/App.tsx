ca095b6b2943afde62f9489cab27243e01aec213
toannq13 <toannq13@fpt.com>
2026-04-21 13:24:56 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import React from 'react';
-import { NavigationContainer, NavigationIndependentTree } from '@react-navigation/native';
+import React from "react";
+import {
+  NavigationContainer,
+  NavigationIndependentTree,
+} from "@react-navigation/native";
 
-import RootNavigator from './navigation/RootNavigator';
-import { AppProps } from './navigation/types';
+import RootNavigator from "./navigation/RootNavigator";
+import {AppProps} from "./navigation/types";
 
-export type { AppProps };
+export type {AppProps};
 
 export default function App(props: AppProps): React.ReactElement {
   return (
     <NavigationIndependentTree>
       <NavigationContainer
         onUnhandledAction={(action) => {
-          if (action.type === 'GO_BACK' && props.onBack) {
+          if (action.type === "GO_BACK" && props.onBack) {
             props.onBack();
           }
-        }}>
+        }}
+      >
         <RootNavigator {...props} />
       </NavigationContainer>
     </NavigationIndependentTree>
